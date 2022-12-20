6c2868a1a8118f36af0b494911cfb6c9c1d732fb
azkannawal <azkannawalaf@gmail.com>
2022-12-20 18:33:57 +0700

    rombak

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'memorandom.html'],
   theme: {
     extend: {
       colors: {
