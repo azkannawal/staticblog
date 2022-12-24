cd346758156c57ac73acc62e048dd644b5fc6ca6
azkannawal <azkannawalaf@gmail.com>
2022-12-24 17:36:18 +0700

    bg2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'sea1': 'url(/dist/img/dua.svg)',
-        'sea2': 'url(/dist/img/rect14695.svg)',
-        'sea3': 'url("https://images.unsplash.com/photo-1503177847378-d2048487fa46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80")'
+        'sea2': 'url("https://images.unsplash.com/photo-1503177847378-d2048487fa46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80")'
       },
       colors: {
         prim: '#1F1F1F',
