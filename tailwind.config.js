269d083888206e45f4625ce0c9de3b00ea426ccc
azkannawal <azkannawalaf@gmail.com>
2022-12-26 23:05:09 +0700

    minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sea1': 'url(/dist/img/dua.svg)',
+        'sea1': 'url(/dist/img/memorandom.svg)',
         'sea2': 'url("https://images.unsplash.com/photo-1503177847378-d2048487fa46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80")'
       },
       colors: {
